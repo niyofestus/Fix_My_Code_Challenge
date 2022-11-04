716bd1fada3ad6140fea049f2e78b798188810d3
NIYITANGA Festus <43537970+niyofestus@users.noreply.github.com>
2022-11-04 15:47:33 +0200

    Update 1-print_square.js

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -14,7 +14,7 @@ if (process.argv.length <= 2) {
     process.exit(1)
 }
 
-size = parseInt(process.argv[2], 16)
+size = parseInt(process.argv[2], 10)
 
 for (let i = 0 ; i < size ; i ++) {
     for (let j = 0 ; j < size ; j ++) {
@@ -22,3 +22,4 @@ for (let i = 0 ; i < size ; i ++) {
     }
     process.stdout.write("\n");
 }
+Footer
